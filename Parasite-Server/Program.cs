0c1c1b7398cd703b88502a9cc50e32f95e54a6c0
Nick <blizzard@gmail.com>
2018-11-12 11:39:50 -0500

    Cleaned up the send and receive code a bit.

--- Parasite-Server/Program.cs
+++ Parasite-Server/Program.cs
@@ -1,8 +1,6 @@
 using System;
 using System.IO;
 using System.IO.Pipes;
-using System.Linq;
-using System.Threading;
 
 namespace Parasite
 {
@@ -19,7 +17,7 @@ namespace Parasite
                 NamedPipeServerStream namedPipeServerStream = new NamedPipeServerStream("RetroArchParasite");
                 namedPipeServerStream.WaitForConnection();
 
-                using (FileStream fileStream = new FileStream("test.state", FileMode.Append))
+                using (FileStream fileStream = new FileStream("../../../states/Super Mario Bros..state9", FileMode.Append))
                 {
                     int readByteCount;
                     do
@@ -30,39 +28,7 @@ namespace Parasite
                     } while (readByteCount != 0);
                 }
 
-                //while (true)
-                //{
-                //    int readByteCount = namedPipeServerStream.Read(readBuffer, 0, readBuffer.Length);
-                //    if (readByteCount == 0)
-                //    {
-                //        Thread.Sleep(10);
-                //    }
-                //    else
-                //    {
-                //        Console.WriteLine("Bytes read!!!: " + readByteCount);
-                //    }
-                //}
-                //namedPipeServerStream.IsMessageComplete
-
-                //File.WriteAllBytes("test.state", readBuffer);
-                //File.
-                //StreamReader streamReader = new StreamReader(namedPipeServerStream);
-
-                //Console.WriteLine("Waiting for data...");
-                //while (streamReader.Peek() >= 0)
-                //{
-                //    Console.WriteLine("Reading block of data...");
-                //    streamReader.ReadBlock(readBuffer, 0, readBuffer.Length);
-                //    //Console.WriteLine(readBuffer);
-                //}
-                //Console.WriteLine("All data read.");
-
-                //byte[] readBufferBytes = readBuffer.Select(c => (byte)c).ToArray();
-
-                //File.WriteAllBytes("test.state", readBufferBytes);
-
                 Console.WriteLine("Closing named pipe...");
-                //streamReader.Close();
                 namedPipeServerStream.Close();
 
                 Console.WriteLine("Reinitializing named pipe...");
--- parasite/parasite.c
+++ parasite/parasite.c
@@ -7,82 +7,29 @@
 
 void parasite_test(void)
 {
-   // RARCH_LOG("parasite_test()\n");
-
-   // char *message = "Hello Pipe";
-   // FILE *file = fopen("\\\\.\\pipe\\RetroArchParasite", "w");
-   // fwrite(message, sizeof(char), strlen(message) + 1, file);
-   // fclose(file);
-
-   // RARCH_LOG("DONE\n");
-
-   /*
-   retro_ctx_size_info_t info;
-   void *data  = NULL;
-
-   core_serialize_size(&info);
-
-   if (info.size == 0)
-      return false;
-
-      RARCH_LOG("%s: \"%s\".\n",
-            msg_hash_to_str(MSG_SAVING_STATE),
-            path);
-
-   data = get_serialized_data(path, info.size) ;
-
-
-      if (!data)
-      {
-         RARCH_ERR("%s \"%s\".\n",
-               msg_hash_to_str(MSG_FAILED_TO_SAVE_STATE_TO),
-               path);
-         return false;
-      }
-
-      RARCH_LOG("%s: %d %s.\n",
-            msg_hash_to_str(MSG_STATE_SIZE),
-            (int)info.size,
-            msg_hash_to_str(MSG_BYTES));
-   */
-
-   RARCH_LOG("HERE 1\n");
-
-   retro_ctx_size_info_t info;
-   core_serialize_size(&info);
-
-   if (info.size == 0)
+   retro_ctx_size_info_t sizeInfo;
+   core_serialize_size(&sizeInfo);
+   if (sizeInfo.size == 0)
       return;
 
-   RARCH_LOG("HERE 2: %d\n", info.size);
-
-   retro_ctx_serialize_info_t serial_info;
-   bool ret    = false;
-   void *data  = NULL;
-
-   data = malloc(info.size);
-
+   void *data = NULL;
+   data = malloc(sizeInfo.size);
    if (!data)
       return;
 
-   RARCH_LOG("HERE 3\n");
+   retro_ctx_serialize_info_t serializeInfo;
+   serializeInfo.data = data;
+   serializeInfo.size = sizeInfo.size;
 
-   serial_info.data = data;
-   serial_info.size = info.size;
-   ret = core_serialize(&serial_info);
-   if (!ret)
+   if (!core_serialize(&serializeInfo))
    {
       free(data);
       return;
    }
 
-   RARCH_LOG("HERE 4\n");
-
-   FILE *file = fopen("\\\\.\\pipe\\RetroArchParasite", "w");
-   fwrite(data, sizeof(char), info.size, file);
+   FILE *file = fopen("\\\\.\\pipe\\RetroArchParasite", "wb");
+   fwrite(data, sizeof(char), sizeInfo.size, file);
    fclose(file);
 
-   RARCH_LOG("DONE\n");
-
-   //return data;
+   free(data);
 }
