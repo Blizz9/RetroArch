e41b05ff7ae7fa35fb73ad939bafa459a5346695
Nick <blizzard@gmail.com>
2018-11-14 07:28:10 -0500

    More work on learning this named pipe stuff.

--- Parasite-Server/Program.cs
+++ Parasite-Server/Program.cs
@@ -1,6 +1,10 @@
 using System;
 using System.IO;
 using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Parasite
 {
@@ -8,31 +12,120 @@ namespace Parasite
     {
         static void Main(string[] args)
         {
-            byte[] readBuffer = new byte[1024];
+            NamedPipeServerStream namedPipeServerStream = new NamedPipeServerStream("RetroArchParasite", PipeDirection.InOut, 1, PipeTransmissionMode.Byte);
 
-            while (true)
+            Task.Factory.StartNew(() =>
             {
-                Console.WriteLine("Waiting for client pipe connection...");
-
-                NamedPipeServerStream namedPipeServerStream = new NamedPipeServerStream("RetroArchParasite");
                 namedPipeServerStream.WaitForConnection();
 
-                using (FileStream fileStream = new FileStream("../../../states/Super Mario Bros..state9", FileMode.Append))
+                byte[] readBuffer = new byte[9];
+                while (true)
                 {
-                    int readByteCount;
-                    do
+                    int readByteCount = namedPipeServerStream.Read(readBuffer, 0, readBuffer.Length);
+
+                    if (readByteCount > 0)
                     {
-                        readByteCount = namedPipeServerStream.Read(readBuffer, 0, readBuffer.Length);
-                        Console.WriteLine("Read bytes: " + readByteCount);
-                        fileStream.Write(readBuffer, 0, readByteCount);
-                    } while (readByteCount != 0);
+                        Console.WriteLine("Server bytes read");
+
+                        byte command = readBuffer[0];
+                        ulong size = BitConverter.ToUInt64(readBuffer, 1);
+
+                        Console.WriteLine("Writing from server");
+                        string writeString = "Sending" + size.ToString();
+                        byte[] writeBuffer = ASCIIEncoding.ASCII.GetBytes(writeString);
+                        namedPipeServerStream.Write(writeBuffer, 0, writeBuffer.Length);
+                    }
+
+                    Thread.Sleep(1);
                 }
+            });
 
-                Console.WriteLine("Closing named pipe...");
-                namedPipeServerStream.Close();
+            while (true) {}
 
-                Console.WriteLine("Reinitializing named pipe...");
-            }
+            //NamedPipeClientStream namedPipeClientStream = new NamedPipeClientStream(".", "RetroArchParasite", PipeDirection.InOut);
+            //namedPipeClientStream.Connect();
+
+            //while (true)
+            //{
+            //    Thread.Sleep(1000);
+
+            //    Console.WriteLine("Writing from client");
+            //    byte[] writeBuffer = Enumerable.Repeat((byte)0x41, 1024).ToArray();
+            //    namedPipeClientStream.Write(writeBuffer, 0, writeBuffer.Length);
+
+            //    while (true)
+            //    {
+            //        byte[] readBuffer = new byte[1024];
+            //        int readByteCount = namedPipeClientStream.Read(readBuffer, 0, readBuffer.Length);
+
+            //        if (readByteCount > 0)
+            //        {
+            //            Console.WriteLine("Client bytes read");
+            //            break;
+            //        }
+
+            //        Thread.Sleep(1);
+            //    }
+            //}
+
+            //byte[] readBuffer = new byte[1024];
+
+            //while (true)
+            //{
+            //    Console.WriteLine("Waiting for client pipe connection...");
+
+            //    NamedPipeServerStream namedPipeServerStream = new NamedPipeServerStream("RetroArchParasite");
+            //    namedPipeServerStream.WaitForConnection();
+
+            //    using (FileStream fileStream = new FileStream("../../../states/Super Mario Bros..state9", FileMode.Append))
+            //    {
+            //        int readByteCount;
+            //        do
+            //        {
+            //            readByteCount = namedPipeServerStream.Read(readBuffer, 0, readBuffer.Length);
+            //            Console.WriteLine("Read bytes: " + readByteCount);
+            //            fileStream.Write(readBuffer, 0, readByteCount);
+            //        } while (readByteCount != 0);
+            //    }
+
+            //    Console.WriteLine("Closing named pipe...");
+            //    namedPipeServerStream.Close();
+
+            //    Console.WriteLine("Reinitializing named pipe...");
+            //}
+
+            /*
+            //byte[] writeBuffer = new byte[1024];
+            byte[] writeBuffer = Enumerable.Repeat((byte)0x65, 1024).ToArray();
+
+            NamedPipeClientStream namedPipeClientStream = new NamedPipeClientStream("RetroArchSend");
+            namedPipeClientStream.Connect();
+            namedPipeClientStream.Write(writeBuffer, 0, writeBuffer.Length);
+
+            namedPipeClientStream.Close();
+            */
+
+            //using (NamedPipeClientStream namedPipeClient = new NamedPipeClientStream(".", "test-pipe", PipeDirection.InOut))
+            //{
+            //    namedPipeClient.Connect();
+            //    Console.WriteLine("Client connected to the named pipe server. Waiting for server to send the first message...");
+            //    namedPipeClient.ReadMode = PipeTransmissionMode.Message;
+            //    string messageFromServer = ProcessSingleReceivedMessage(namedPipeClient);
+            //    Console.WriteLine("The server is saying {0}", messageFromServer);
+            //    Console.Write("Write a response: ");
+            //    string response = Console.ReadLine();
+            //    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+            //    namedPipeClient.Write(responseBytes, 0, responseBytes.Length);
+            //    while (response != "x")
+            //    {
+            //        messageFromServer = ProcessSingleReceivedMessage(namedPipeClient);
+            //        Console.WriteLine("The server is saying {0}", messageFromServer);
+            //        Console.Write("Write a response: ");
+            //        response = Console.ReadLine();
+            //        responseBytes = Encoding.UTF8.GetBytes(response);
+            //        namedPipeClient.Write(responseBytes, 0, responseBytes.Length);
+            //    }
+            //}
         }
     }
 }
--- parasite/parasite.c
+++ parasite/parasite.c
@@ -1,35 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <windows.h>
 
 #include "../core.h"
 #include "../verbosity.h"
 
+HANDLE parasitePipe;
+
 void parasite_test(void)
 {
-   retro_ctx_size_info_t sizeInfo;
-   core_serialize_size(&sizeInfo);
-   if (sizeInfo.size == 0)
-      return;
+   // retro_ctx_size_info_t sizeInfo;
+   // core_serialize_size(&sizeInfo);
+   // if (sizeInfo.size == 0)
+   //    return;
 
-   void *data = NULL;
-   data = malloc(sizeInfo.size);
-   if (!data)
-      return;
+   // void *data = NULL;
+   // data = malloc(sizeInfo.size);
+   // if (!data)
+   //    return;
 
-   retro_ctx_serialize_info_t serializeInfo;
-   serializeInfo.data = data;
-   serializeInfo.size = sizeInfo.size;
+   // retro_ctx_serialize_info_t serializeInfo;
+   // serializeInfo.data = data;
+   // serializeInfo.size = sizeInfo.size;
 
-   if (!core_serialize(&serializeInfo))
+   // if (!core_serialize(&serializeInfo))
+   // {
+   //    free(data);
+   //    return;
+   // }
+
+   // FILE *file = fopen("\\\\.\\pipe\\RetroArchParasite", "wb");
+   // fwrite(data, sizeof(char), sizeInfo.size, file);
+   // fclose(file);
+
+   // free(data);
+
+   if (parasitePipe == NULL)
    {
-      free(data);
-      return;
+      RARCH_LOG("Parasite pipe is NULL, initializing it.\n");
+      parasitePipe = CreateFile(TEXT("\\\\.\\pipe\\RetroArchParasite"), GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+      if (parasitePipe == INVALID_HANDLE_VALUE)
+      {
+         RARCH_LOG("ERROR: Unable ot create parasite pipe.\n");
+      }
+   }
+   else
+   {
+      RARCH_LOG("Parasite pipe is already initialized.\n");
    }
 
-   FILE *file = fopen("\\\\.\\pipe\\RetroArchParasite", "wb");
-   fwrite(data, sizeof(char), sizeInfo.size, file);
-   fclose(file);
+   retro_ctx_size_info_t sizeInfo;
+   core_serialize_size(&sizeInfo);
 
-   free(data);
+   uint8_t commandByte[1] = { 0x01 };
+
+   uint8_t sizeBytes[sizeof(sizeInfo.size)];
+   for (int i = 0; i < sizeof(sizeInfo.size); i++)
+      sizeBytes[i] = (sizeInfo.size >> (i * CHAR_BIT));
+
+   int headerSize = sizeof(commandByte) + sizeof(sizeBytes);
+   uint8_t headerBytes[headerSize];
+   memcpy(headerBytes, commandByte, sizeof(commandByte));
+   memcpy(headerBytes + sizeof(commandByte), sizeBytes, sizeof(sizeBytes));
+
+   DWORD writtenByteCount;
+   WriteFile(parasitePipe, &headerBytes, sizeof(headerBytes), &writtenByteCount, NULL);
+   RARCH_LOG("Wrote to parasite pipe.\n");
+
+   char readBuffer[12];
+   DWORD dwRead;
+   RARCH_LOG("Reading from parasite pipe...\n");
+   // while (ReadFile(parasitePipe, readBuffer, sizeof(readBuffer), &dwRead, NULL) != FALSE)
+   // {
+   //    RARCH_LOG("Read data from parasite pipe.\n");
+   //    RARCH_LOG("%s\n", readBuffer);
+   //    break;
+   // }
+
+   ReadFile(parasitePipe, readBuffer, sizeof(readBuffer), &dwRead, NULL);
+   RARCH_LOG("Read data from parasite pipe.\n");
+   RARCH_LOG("%s\n", readBuffer);
 }
